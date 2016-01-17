2d20edf0ee847b852509c26b53f2c3cb630b38b5
mrmeatloaf@gmail.com <mrmeatloaf@gmail.com>
2016-01-17 16:19:51 -0500

    Updated SQL to include create schema for all tables

--- webapp/src/main/sql/create.sql
+++ webapp/src/main/sql/create.sql
@@ -1,20 +1,94 @@
-create table user_role (
+-- groups such as: admin, owner, employee, user, mayor, etc.
+create table roles (
     id serial primary key,
-    role varchar(100) NOT NULL,
-    description text NOT NULL
+    name varchar(100) NOT NULL,
+    info text NOT NULL
 )
 ;
 
-insert into user_role (role, description) values('ROLE_USER', 'Customer');
-insert into user_role (role, description) values('ROLE_OWNER', 'Bar Owner');
-insert into user_role (role, description) values('ROLE_ADMIN', 'Admin');
-insert into user_role (role, description) values('ROLE_STAFF', 'Bartender');
-insert into user_role (role, description) values('ROLE_VIEW', 'Read Only View');
-    
+insert into roles (name, info) values('ADMIN', 'Administrator');
+insert into roles (name, info) values('OWNER', 'Owner');
+insert into roles (name, info) values('MANAGER', 'Manager');
+insert into roles (name, info) values('EMPLOYEE', 'Employee');
+insert into roles (name, info) values('USER', 'Standard User');
+insert into roles (name, info) values('KIOSK', 'POS Kiosk');
+insert into roles (name, info) values('DISPLAY', 'Read-only User');
+
+-- users of the system. each has account info and associated role_id
 create table users (
     id serial primary key,
-    username varchar(100) NOT NULL,
-    password varchar(100) NOT NULL,
-    role_id integer references user_role(id)
+    name varchar(100) NOT NULL,
+    password_hash varchar(100) NOT NULL,
+    password_salt varchar(100) NOT NULL,
+    info text NOT NULL,
+    icon_url text NOT NULL,
+    email text NOT NULL,
+    phone NOT NULL,
+    zip NOT NULL,
+    role_id integer references roles(id)
+)
+;
+
+-- these are the overlying companies that own the individual bars.
+create table business (
+    id serial primary key,
+    name varchar(100) NOT NULL,
+    owner_id references users(id),
+    contact_info varchar(100) NOT NULL,
+    info text NOT NULL,
+    icon_url text NOT NULL
+)
+;
+
+-- these are the individual bars. (eg. a local establishment or chain restaurant)
+create table bar (
+    id serial primary key,
+    name varchar(100) NOT NULL,
+    business_id references business(id),
+    location varchar(100) NOT NULL,
+    info text NOT NULL,
+    icon_url text NOT NULL,
+    special_users text NOT NULL -- This field will be a concatenation of multiple 'users.id'.
+)
+;
+
+-- this is an individual pickup location at a bar.
+create table queue (
+    id serial primary key,
+    name varchar(100) NOT NULL,
+    bar_id references bar(id)
+)
+;
+
+-- this is to be considered as the receipt. stores drink orders, order and pickup timestamps.
+create table drinkorder (
+    id serial primary key,
+    info json NOT NULL, -- This field will contain the session information about drinks that were ordered. 
+    queue_id references queue(id),
+    user_id references users(id),
+    bar_id references bar(id)
+    order_time timestamp NOT NULL, -- This should be updated once the order is verified complete.
+    pickup_time timestamp NOT NULL  -- This to be updated once the pickup is verified
+)
+;
+-- table of individual drinks tied to each queue. 10oz pour, 20oz pour, pint, martini, rum & coke, $4 shot, $5 shot, etc.
+create table drink (
+    id serial primary key,
+    name varchar(100) NOT NULL,
+    info text NOT NULL,
+    items text NOT NULL, -- This will be a concatenation of 1 or more item(id) 
+    make_time smallint NOT NULL,
+    icon_url text NOT NULL,	
+    queue_id references queue(id)
+)
+;
+
+-- contains each individual items. individual beers, liquors, fruits, garnishes, etc.
+create table item (
+    id serial primary key,
+    name varchar(100) NOT NULL,
+    info text NOT NULL,
+    icon_url text NOT NULL,
+    make_time smallint NOT NULL
 )
 ;
