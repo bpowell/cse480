cab74be4fc94027b62ba57d4c0d7b71a7d1ba1dd
jelevoir <jelevoir@oakland.edu>
2016-01-24 21:10:44 -0500

    Add User model.

---
+++ webapp/src/main/java/edu/oakland/cse480/mvc/models/User.java
@@ -0,0 +1,12 @@
+package edu.oakland.cse480.mvc.models;
+
+public class User {
+        public int id;
+        public String name;
+        public String email;
+
+        public User() {}
+        public int getId() {return id;}
+        public String getName() {return name;}
+        public String getEmail() {return email;}
+}
