1ea630c23c2996fcaafc6807841fb233e339d591
Brandon Powell <bpowell29a@gmail.com>
2016-01-25 22:16:06 -0500

    Wired up /login to the controller

--- webapp/src/main/java/edu/oakland/cse480/mvc/controller/MainController.java
+++ webapp/src/main/java/edu/oakland/cse480/mvc/controller/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
@@ -33,4 +35,19 @@ public class MainController{
         return "index";
     }
 
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username or password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+
+		return model;
+	}
 }
