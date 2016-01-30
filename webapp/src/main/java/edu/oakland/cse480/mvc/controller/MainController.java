73e938882b839a977b179cb585c3e81855a7173a
jelevoir <jelevoir@oakland.edu>
2016-01-30 18:11:02 -0500

    Formatting of MainController.java

--- webapp/src/main/java/edu/oakland/cse480/mvc/controller/MainController.java
+++ webapp/src/main/java/edu/oakland/cse480/mvc/controller/MainController.java
@@ -41,21 +41,21 @@ public class MainController{
         return "index";
     }
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) String logout) {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) String logout) {
 
-		ModelAndView model = new ModelAndView();
-		if (error != null) {
-			model.addObject("error", "Invalid username or password!");
-		}
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username or password!");
+        }
 
-		if (logout != null) {
-			model.addObject("msg", "You've been logged out successfully.");
-		}
-		model.setViewName("login");
+        if (logout != null) {
+            model.addObject("msg", "You've been logged out successfully.");
+        }
+        model.setViewName("login");
 
-		return model;
-	}
+        return model;
+    }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String register() {
