3be19d1e7f75505d5bc746f3655680f392b95da6
Brandon Powell <bpowell29a@gmail.com>
2016-01-30 23:02:15 -0500

    Part 1 of example service.
    Added service for categories

---
+++ webapp/src/main/java/edu/oakland/cse480/mvc/models/Categories.java
@@ -0,0 +1,31 @@
+package edu.oakland.cse480.mvc.models;
+
+public class Categories {
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
