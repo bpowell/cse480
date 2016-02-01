4e8e65099c985afa5db13d9beedfac27ae466477
jelevoir <jelevoir@oakland.edu>
2016-01-31 22:59:53 -0500

    Removed setId method

--- webapp/src/main/java/edu/oakland/cse480/mvc/models/User.java
+++ webapp/src/main/java/edu/oakland/cse480/mvc/models/User.java
@@ -1,12 +1,31 @@
 package edu.oakland.cse480.mvc.models;
 
 public class User {
-        public int id;
-        public String name;
-        public String email;
+    public int id;
+    public String name;
+    public String email;
 
-        public User() {}
-        public int getID() {return id;}
-        public String getName() {return name;}
-        public String getEmail() {return email;}
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
