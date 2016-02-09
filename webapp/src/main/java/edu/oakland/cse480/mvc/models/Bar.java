6488afd8379e3851ab1c747b00baba6a63e0fa58
Brandon Powell <bpowell29a@gmail.com>
2016-02-09 15:53:58 -0500

    Changed business_id to int

--- webapp/src/main/java/edu/oakland/cse480/mvc/models/Bar.java
+++ webapp/src/main/java/edu/oakland/cse480/mvc/models/Bar.java
@@ -3,7 +3,7 @@ package edu.oakland.cse480.mvc.models;
 public class Bar {
     public int id;
     public String name;
-    public String business_id;
+    public int business_id;
 
     public int getId() {
         return this.id;
@@ -17,11 +17,11 @@ public class Bar {
         this.name = name;
     }
 
-    public String getBusinessId() {
+    public int getBusinessId() {
         return this.business_id;
     }
 
-    public void setBusinessId(String business_id) {
+    public void setBusinessId(int business_id) {
         this.business_id = business_id;
     }
 }
