f89d34a230d66a9194cd76994031da377dddfe19
Brandon Powell <bpowell29a@gmail.com>
2016-02-14 22:42:31 -0500

    Combined bar and business services
    Because bars and businesses are closely related, I merged the two
    services into one.

--- webapp/src/main/java/edu/oakland/cse480/mvc/models/Business.java
+++ webapp/src/main/java/edu/oakland/cse480/mvc/models/Business.java
@@ -1,10 +1,18 @@
 package edu.oakland.cse480.mvc.models;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Business {
     private int id;
     private String name;
     private String info;
     private String icon_url;
+    private List<Bar> bars;
+
+    public Business() {
+        this.bars = new ArrayList<Bar>();
+    }
 
     public void setId(int id) {
         this.id = id;
@@ -36,5 +44,17 @@ public class Business {
 
     public void setIconUrl(String icon_url) {
         this.icon_url = icon_url;
+    }
+
+    public void setBars(List<Bar> bars) {
+        this.bars = bars;
+    }
+
+    public List<Bar> getBars() {
+        return this.bars;
+    }
+
+    public void addBar(Bar b) {
+        this.bars.add(b);
     }
 }
--- webapp/src/main/java/edu/oakland/cse480/service/BusinessAndBarService.java
+++ webapp/src/main/java/edu/oakland/cse480/service/BusinessAndBarService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 import edu.oakland.cse480.mvc.models.Bar;
+import edu.oakland.cse480.mvc.models.Business;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,7 +15,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.*;
 
 @Service
-public class BarService extends AbstractJdbcDriver {
+public class BusinessAndBarService extends AbstractJdbcDriver {
 	protected final Logger log = LoggerFactory.getLogger(getClass());
 
 	public List<Bar> getAllBars() {
@@ -37,6 +38,30 @@ public class BarService extends AbstractJdbcDriver {
 		}
 	}
 
+	public List<Business> getAllBusinesses() {
+		try {
+			List<Business> b = new ArrayList<Business>();
+			b.addAll(this.jdbcPostgres.query("select * from business", new BusinessMapper()));
+			return b;
+		} catch(Exception er) {
+			throw new IllegalArgumentException(er.getMessage());
+		}
+	}
+
+    public List<Business> getAllBusinessAndBars() {
+        try {
+            List<Business> business = new ArrayList<Business>();
+            business = getAllBusinesses();
+            for (Business b : business) {
+                b.setBars(getBarsByBusinessId(b.getId()));
+            }
+
+            return business;
+        } catch(Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
 	private class BarMapper implements RowMapper<Bar>{
 		public Bar mapRow(ResultSet rs, int rowNum) throws SQLException {
 			Bar r = new Bar();
@@ -46,6 +71,17 @@ public class BarService extends AbstractJdbcDriver {
             r.setOwnerId(rs.getInt("owner_id"));
             r.setContactInfo(rs.getString("contact_info"));
             r.setLocation(rs.getString("location"));
+			return r;
+		}
+	}
+
+	private class BusinessMapper implements RowMapper<Business>{
+		public Business mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Business r = new Business();
+			r.setName(rs.getString("name"));
+			r.setId(rs.getInt("id"));
+			r.setInfo(rs.getString("info"));
+			r.setIconUrl(rs.getString("icon_url"));
 			return r;
 		}
 	}
