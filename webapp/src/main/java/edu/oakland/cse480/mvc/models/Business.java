807fbbe5c90d5b25a1fce59d87929a60e2993243
jelevoir <jelevoir@oakland.edu>
2016-02-15 22:20:49 -0500

    Alter business model to reflect db changes

--- webapp/src/main/java/edu/oakland/cse480/mvc/models/Business.java
+++ webapp/src/main/java/edu/oakland/cse480/mvc/models/Business.java
@@ -3,13 +3,15 @@ package edu.oakland.cse480.mvc.models;
 public class Business {
     public int id;
     public String name;
-    public int owner_id;
-    public String contact_info;
     public String info;
     public String icon_url;
 
     public int getId() {
         return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -18,22 +20,6 @@ public class Business {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getOwnerId() {
-        return this.owner_id;
-    }
-
-    public void setOwnerId(int owner_id) {
-        this.owner_id = owner_id;
-    }
-
-    public String getContactInfo() {
-        return this.contact_info;
-    }
-
-    public void setContactInfo(String contact_info) {
-        this.contact_info = contact_info;
     }
 
     public String getInfo() {
