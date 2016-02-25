b3cd447a63ffc29b48f0b3663cd5d6e212c1baad
Brandon Powell <bpowell29a@gmail.com>
2016-02-25 10:43:13 -0500

    Started framework for admin panel

---
+++ webapp/src/main/java/edu/oakland/cse480/mvc/controller/AdminContoller.java
@@ -0,0 +1,44 @@
+package edu.oakland.cse480.mvc.controller;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+import java.util.Objects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Handles the initial web page loading.
+ */
+@Controller
+@RequestMapping("/admin")
+public class AdminContoller {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     * Sends the user to the main page.
+     *
+     * @param model
+     * @return The index jsp page.
+     */
+    @RequestMapping("/")
+    public String getIndex(Model model){
+        return "admin";
+    }
+}
---
+++ webapp/src/main/webapp/WEB-INF/jsp/admin.jsp
@@ -0,0 +1,5 @@
+<jsp:directive.include file="/WEB-INF/jsp/header.jsp"/>
+    <body>
+        <h1>Hello Admin</h1>
+    </body>
+</html>
