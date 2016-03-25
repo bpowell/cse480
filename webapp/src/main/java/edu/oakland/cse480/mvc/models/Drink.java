c5fb2741e23bb52813d0e862862c278a54467f48
Brandon Powell <bpowell29a@gmail.com>
2016-03-24 21:56:09 -0400

    Added list of ingredients to drink model

--- webapp/src/main/java/edu/oakland/cse480/mvc/models/Drink.java
+++ webapp/src/main/java/edu/oakland/cse480/mvc/models/Drink.java
@@ -1,11 +1,19 @@
 package edu.oakland.cse480.mvc.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Drink {
     public int id;
     public String name;
     public String info;
     public int make_time;
     public String icon_url;
+    private List<Ingredient> ingredients;
+
+    public Drink() {
+        this.ingredients = new ArrayList<Ingredient>();
+    }
 
     public int getId() {
         return this.id;
@@ -45,5 +53,13 @@ public class Drink {
 
     public void setIconUrl(String icon_url) {
         this.icon_url = icon_url;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return this.ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }   
