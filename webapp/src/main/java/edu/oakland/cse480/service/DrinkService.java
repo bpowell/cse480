d009d518d423140ff39a21b416acf1f312d031fc
Brandon Powell <bpowell29a@gmail.com>
2016-03-26 15:32:47 -0400

    Drink can be added and all ingredients required

--- webapp/src/main/java/edu/oakland/cse480/service/DrinkService.java
+++ webapp/src/main/java/edu/oakland/cse480/service/DrinkService.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.core.simple.*;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import edu.oakland.cse480.mvc.models.Drink;
+import edu.oakland.cse480.mvc.models.Ingredient;
 
 @Service
 public class DrinkService extends AbstractJdbcDriver {
@@ -69,12 +70,18 @@ public class DrinkService extends AbstractJdbcDriver {
         }
     }
 
-    public void insertDrink(Drink d) {
+    public int insertDrink(Drink d) {
+        int did = 0;
         try {
-            this.jdbcPostgres.update("insert into drink (name, info, make_time, icon_url) values(?, ?, ?, ?)", new Object[] {d.getName(), d.getInfo(), d.getMakeTime(), d.getIconUrl()});
+            did = this.jdbcPostgres.queryForObject("insert into drink (name, info, make_time, icon_url) values(?, ?, ?, ?) returning id", Integer.class, new Object[] {d.getName(), d.getInfo(), d.getMakeTime(), d.getIconUrl()});
+            for(Ingredient i : d.getIngredients()) {
+                this.jdbcPostgres.update("insert into drink_ingredients (drink_id, ingredient_id) values(?, ?)", new Object[] {did, i.getId()});
+            }
         } catch(Exception e) {
             log.info("Cannot insert drink");
         }
+
+        return did;
     }
 
     public void deleteDrinkById(int id) {
