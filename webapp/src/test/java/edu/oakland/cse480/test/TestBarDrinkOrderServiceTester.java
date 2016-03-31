becfba017e8141d787dd1971ba036efdee600497
Don <dcfreema@oakland.edu>
2016-03-31 19:06:08 -0400

    Create BarDrinkOrderServiceTester #124

---
+++ webapp/src/test/java/edu/oakland/cse480/test/TestBarDrinkOrderServiceTester.java
@@ -0,0 +1,56 @@
+package edu.oakland.cse480.test;
+
+import org.junit.Assert;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import edu.oakland.cse480.service.BarDrinkOrderService;
+import edu.oakland.cse480.mvc.models.BarDrinkOrder;
+
+public class TestBarDrinkOrderServiceTester {
+        private static EmbeddedDatabase db;
+        private static BarDrinkOrderService barDrinkOrder;
+
+        @BeforeClass
+        public static void setUp() {
+                db = new EmbeddedDatabaseBuilder()
+                        .setType(EmbeddedDatabaseType.HSQL)
+                        .addScript("sql/hsql.sql")
+                        .addScript("sql/create.sql")
+                        .addScript("sql/mock.sql")
+                        .build();
+
+                barDrinkOrder = new BarDrinkOrderService();
+               	barDrinkOrder.setPostgresDataSource(db);
+        }
+
+        @Test
+        public void testGetDrinkOrdersByBarId() {
+                Assert.assertEquals(barDrinkOrder.getDrinkOrdersByBarId(2).size(), 0);
+        }
+
+        @Test
+        public void testGetThreeDrinkOrdersByBarId() {
+                Assert.assertEquals(barDrinkOrder.getThreeDrinkOrdersByBarId(2).size(), 0);
+        }
+
+        @Test
+        public void testGetDrinkOrdersByUsername() {
+                Assert.assertEquals(barDrinkOrder.getDrinkOrdersByUsername("Don").size(), 1);
+        }
+
+        @Test
+        public void testGetFiveDrinksByEmail() {
+                Assert.assertEquals(barDrinkOrder.getFiveDrinksByEmail("don@don.com").size(), 1);
+        }
+
+        @AfterClass
+        public static void shutdown() {
+                db.shutdown();
+        }
+}
